6f521f267c873898e678cb59af3a9fe26bad7e00
Galad Dirie <mahamed.idil@gmail.com>
2025-03-09 03:40:14 -0400

    Project setup

---
+++ backend/backend/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+from rest_framework.routers import DefaultRouter
+from portfolio.views import ProjectViewSet
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
+
+def home_view(request):
+    return HttpResponse("<h1>Welcome to My Django API</h1>")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('', home_view),  # This makes http://127.0.0.1:8000/ show a welcome message.
+]
